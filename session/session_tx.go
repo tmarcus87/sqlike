31f1ed1c78b53f6864aa8e9a2a1daea721c3f235
Takahiko ONO <t.marcus1987@gmail.com>
2020-04-27 11:53:02 +0900

    Fix TxSession

--- session/session_tx.go
+++ session/session_tx.go
@@ -41,6 +41,10 @@ func (s *basicTxSession) Explain() statement.ExplainSelectBranchStep {
 	return statement.NewExplainSelectBranchStep(s.rootStep())
 }
 
+func (s *basicTxSession) Query(stmt string, bindings []interface{}) statement.Statement {
+	return statement.NewInstantStep(s.rootStep(), stmt, bindings)
+}
+
 func (s *basicTxSession) SelectOne() statement.SelectOneBranchStep {
 	return statement.NewSelectOneBranchStep(s.rootStep())
 }
